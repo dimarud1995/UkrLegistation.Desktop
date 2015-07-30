8f308435f23f983b8cd084040abbb2b4936f7037
inform304@gmail.com <inform304@gmail.com>
2015-07-30 16:25:02 +0300

    Authorization in process.

--- UkrLegistation.Desktop/Json/UserJson.cs
+++ UkrLegistation.Desktop/Json/UserJson.cs
@@ -1,18 +1,36 @@
 ﻿
+using System;
+using System.IO;
 using System.Net;
-using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using UkrLegistation.Desktop.Model;
 
 namespace UkrLegistation.Desktop.Json
 {
     public static class UserJson
     {
-        public static void GetData()
+        public static User UserData = new User();
+        public static void GetData(out User user)
         {
             var url = "http://ukrlegislation-itevent.rhcloud.com/restserver/user/1/";
 
-            var syncClient = new WebClient();
-            var content = syncClient.DownloadString(url);
+            var asyncClient = new WebClient();
 
+            asyncClient.DownloadStringCompleted += asyncClient_DownloadStringComleted;
+            asyncClient.DownloadStringAsync(new Uri(url));
+
+            user = UserData;
+        }
+
+        private static void asyncClient_DownloadStringComleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(User));
+
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(e.Result)))
+            {
+                UserData = (User) serializer.ReadObject(ms);
+            }
         }
     }
 }
