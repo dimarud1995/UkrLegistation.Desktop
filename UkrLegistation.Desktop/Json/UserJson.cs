4ce4c0df295fe8b983934d682108d2b1538aba7e
inform304@gmail.com <inform304@gmail.com>
2015-07-30 10:32:53 +0300

    Parsing json data

---
+++ UkrLegistation.Desktop/Json/UserJson.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace UkrLegistation.Desktop.Json
+{
+    public static class UserJson
+    {
+        public static void GetData()
+        {
+            var url = "http://ukrlegislation-itevent.rhcloud.com/restserver/user/1/";
+
+            var syncClient = new WebClient();
+            var content = syncClient.DownloadString(url);
+
+        }
+    }
+}
