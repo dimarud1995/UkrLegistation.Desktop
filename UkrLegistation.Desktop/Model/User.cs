16def993594f93b721cb01f1eb233ddd6d12fbf2
inform304@gmail.com <inform304@gmail.com>
2015-07-30 16:26:21 +0300

    Add json in model.

--- UkrLegistation.Desktop/Model/User.cs
+++ UkrLegistation.Desktop/Model/User.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
+
 
 namespace UkrLegistation.Desktop.Model
 {
+    [DataContract]
     public class User
     {
-        public int Id { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
-        public string FullName { get; set; }
-        public long RegistrationDate { get; set; }
-        public string Role { get; set; }
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string login { get; set; }
+        [DataMember]
+        public string password { get; set; }
+        [DataMember]
+        public string fullName { get; set; }
+        [DataMember]
+        public long registrationDate { get; set; }
+        [DataMember]
+        public string role { get; set; }
     }
 }
