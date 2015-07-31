ff298d6b4ecff07e3a29aecb3b6e0beaac124309
inform304@gmail.com <inform304@gmail.com>
2015-07-31 11:24:28 +0300

    Add error check.

--- UkrLegistation.Desktop/Json/UserJson.cs
+++ UkrLegistation.Desktop/Json/UserJson.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Runtime.Serialization.Json;
@@ -10,26 +11,37 @@ namespace UkrLegistation.Desktop.Json
 {
     public static class UserJson
     {
-        public static User UserData = new User();
-        public static void GetData(out User user)
+        public static List<User> UserData = new List<User>();
+        private static bool _flag;
+        public static void GetData(out List<User> users, out bool flagNew)
         {
-            var url = "http://ukrlegislation-itevent.rhcloud.com/restserver/user/1/";
+            var url = "http://ukrlegislation-itevent.rhcloud.com/restserver/user/";
 
             var asyncClient = new WebClient();
-
             asyncClient.DownloadStringCompleted += asyncClient_DownloadStringComleted;
             asyncClient.DownloadStringAsync(new Uri(url));
+            flagNew = _flag;
+            users = UserData;
 
-            user = UserData;
+
         }
 
         private static void asyncClient_DownloadStringComleted(object sender, DownloadStringCompletedEventArgs e)
         {
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(User));
-
-            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(e.Result)))
+            try
             {
-                UserData = (User) serializer.ReadObject(ms);
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof (List<User>));
+
+                using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(e.Result)))
+                {
+                    UserData = (List<User>) serializer.ReadObject(ms);
+                }
+            }
+            catch 
+            {
+                Window1 wind = new Window1();
+                wind.Show();
+                _flag = true;
             }
         }
     }
